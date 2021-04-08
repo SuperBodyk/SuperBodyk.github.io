b4469f760e795ffda20982a2783f96ad060ca58c
SuperBodyk <boduk007@gmail.com>
2021-04-08 22:37:53 +0300

    wine

--- JS/menu.js
+++ JS/menu.js
@@ -1,12 +1,30 @@
+var timesClicked=1;
 function FunctionMenu(x) {
 		 x.classList.toggle("change");
           timesClicked++;
          if(timesClicked%2==0)
          {
-             $('.menu_site').removeClass('done');
+             $('.divice_menu').removeClass('done');
         }
          else
          {
-             $('.menu_site').addClass('done');
+             $('.divice_menu').addClass('done');
          }
      }
+function FunctionDone() {
+        timesClicked++;
+        $('.divice_menu').addClass('done');
+        var elem = document.querySelector('#menu_top')
+        elem.classList.toggle("change");
+       
+     }
+$('.menu_site a').on('click', function(e){
+
+        let id = $(this).attr('href');
+        let el = $('#' + id).offset().top;
+        let top = parseInt($(this).attr('data-top'));
+        
+        $("html").animate({ scrollTop: el  + top}, "slow");
+    });
+
+$('.preloader').click(false);
--- JS/preloader.js
+++ JS/preloader.js
@@ -1,7 +1,14 @@
+function noScroll() {
+  window.scrollTo(0, 0);
+}
+window.addEventListener('scroll', noScroll);
+
+
 window.onload =function () {
 	setTimeout(function(){
 		var preoloader = document.getElementById('page-preloader');
 		preoloader.classList.add('done');
+		window.removeEventListener('scroll', noScroll);
 		
 	}, 500);
 	// body...
