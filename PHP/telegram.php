a4cb9595e3a8c91525b40e8797b4b2143dd3d709
SuperBodyk <boduk007@gmail.com>
2021-04-10 22:00:38 +0300

    date_10/04

---
+++ PHP/telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot1245570473:AAH3PdaJh4YZylQKy2zh6q0Itu5ipmob0Ck/getUpdates,
+*/
+
+session_start ();
+$token = "1717742261:AAHAPDEWUZuqCyZetZb4uyVQGeFcFqwFoIk";
+$chat_id = "-371289017";
+   $Cookie_Name=$_COOKIE["Name_people"];
+   $Cookie_Email=$_COOKIE['Email_people'];
+   $Cookie_Phone=$_COOKIE['Phone_people'];
+   $Cookie_Qwest=$_COOKIE['Qwest_people'];
+   $date_for_server=date('d/m/Y');
+   $time_for_server=date('H:i');
+   $txt = "Повідомлення від:".$Cookie_Name." <br>
+   Звернувся на сайті в ".$time_for_server." годині ".$date_for_server." <br>
+   Номер телефону:".$Cookie_Phone." <br> 
+   Емейл адрес:".$Cookie_Email." <br>
+   Запитання від користувача: ".$Cookie_Qwest." .";
+
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+$URL='../index.html';
+if ($sendToTelegram) {
+   header("location: $URL");
+}
+?>
